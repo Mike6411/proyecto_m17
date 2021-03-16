e62541c8852ec461d2bd618499963fcb6ca35852
Ernest Gomis <gxmis791@gmail.com>
2021-03-16 21:11:26 +0100

    Barril ahora si pusheado :v
    Barril hecho :D

---
+++ Assets/Scripts/Barril_Script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barril_Script : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public GameObject explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        GameObject newBoom = Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
