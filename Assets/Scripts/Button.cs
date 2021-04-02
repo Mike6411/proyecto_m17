df6e2229a41c47462d0e052efb2158e4ba00aec1
Sara <sara.perez@enti.cat>
2021-04-02 23:11:57 +0200

    Tutorial + sprite puerta abierta + script boton a medias
    añadido tutoriales a los niveles, sprite de puerta abierta y cambiado en los niveles que se necesitaban que estuvieran abiertas, más nuevo script del botón a medias

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    //poner aqui la animacion de la puerta y que cuando el botton o la placa sean presionadas o tocadas llamar a la animacion de la puerta
+    //para hacer que se active la animacion mientras pongamos un if que colisione con todo con todo ya estaria llamando a la animacion
+
+    private Animator animator;
+    private int doorOpenParamID;
+    public Rigidbody2D rb;
+    bool click;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        click = false;
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        doorOpenParamID = Animator.StringToHash("doorOpen");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Button Interaction
+        if (click)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+
+                bool isOpen = animator.GetBool(doorOpenParamID);
+                animator.SetBool(doorOpenParamID, true);
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            click = true;
+        }
+    }
+}
