dd0450cc691087e2dd9d49fe8a617801a039996b
Sara <sara.perez@enti.cat>
2021-04-02 19:59:18 +0200

    Botón v1
    la animación de cuando le das al botón funciona pero no esta del todo bien el botón

--- Assets/Scripts/Pressure_plate.cs
+++ Assets/Scripts/Pressure_plate.cs
@@ -5,8 +5,53 @@ using UnityEngine;
 public class Pressure_plate : MonoBehaviour
 {
    
- public bool IsDoorOpened = false;
+    //poner aqui la animacion de la puerta y que cuando el botton o la placa sean presionadas o tocadas llamar a la animacion de la puerta
+    //para hacer que se active la animacion mientras pongamos un if que colisione con todo con todo ya estaria llamando a la animacion
+
+    private Animator animator;
+    private int doorOpenParamID;
+    public Rigidbody2D rb;
+    bool click;
 
 
+    private void Start()
+    {
+        click = false;
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        doorOpenParamID = Animator.StringToHash("doorOpen");
+    }
+
+    private void Update()
+    {
+
+        //Button Interaction
+        if (click) { 
+            if (Input.GetKeyDown(KeyCode.E)) { 
+        
+                bool isOpen = animator.GetBool(doorOpenParamID);
+                animator.SetBool(doorOpenParamID, true);
+        
+            }
+        
+        }
+       
  
+    }
+
+    void FixedUpdate()
+    {
+      
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            click = true;
+        }
+
+    }
+
+
+
 }
