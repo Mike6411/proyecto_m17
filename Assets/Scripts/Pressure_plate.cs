0932147f741b342934fcbc531bc2b3c4a32be629
Sara <sara.perez@enti.cat>
2021-04-28 14:40:53 +0200

    Correcciones varias
    modificado el nivel 1 y eliminado el nivel 0

--- Assets/Scripts/Pressure_plate.cs
+++ Assets/Scripts/Pressure_plate.cs
@@ -8,32 +8,41 @@ public class Pressure_plate : MonoBehaviour
   public GameObject door;
   private Animator animator;
     private int doorOpenParamID;
-    public Rigidbody2D rb;
+   // public Rigidbody2D rb;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        
+    //    rb = GetComponent<Rigidbody2D>();
         animator = door.GetComponent<Animator>();
         doorOpenParamID = Animator.StringToHash("doorOpen");
     } 
     
     void OnTriggerStay2D(Collider2D col){
       if (col.gameObject.tag != "Projectile"){
-         bool isOpen = animator.GetBool(doorOpenParamID);
-         animator.SetBool(doorOpenParamID, true);
-         FindObjectOfType<AudioManager>().Play("OpenDoor");
+        // bool isOpen = animator.GetBool(doorOpenParamID);
+                animator.SetBool(doorOpenParamID, true);
+            AudioManager am = FindObjectOfType<AudioManager>();
+            if (am != null) { 
+                am.Play("OpenDoor");
+            }
+            
       }
     }
 
    void OnTriggerExit2D(Collider2D col)
     {
       if (col.gameObject.tag != "Projectile"){
-         bool isOpen = animator.GetBool(doorOpenParamID);
-         animator.SetBool(doorOpenParamID, false);
-         FindObjectOfType<AudioManager>().Play("OpenDoor");
-      }
+        // bool isOpen = animator.GetBool(doorOpenParamID);
+                animator.SetBool(doorOpenParamID, false);
+            AudioManager am = FindObjectOfType<AudioManager>();
+            if (am != null)
+            {
+                am.Play("OpenDoor");
+            }
+        }
                  
     }
 
