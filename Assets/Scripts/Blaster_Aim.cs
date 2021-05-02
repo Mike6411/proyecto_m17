53fc0ae26860acbe0e906b2d736da6de4e70346a
Miquel Nadal <miquel.nadal@enti.cat>
2021-05-02 02:18:04 +0200

    Cooldown de armas

--- Assets/Scripts/Blaster_Aim.cs
+++ Assets/Scripts/Blaster_Aim.cs
@@ -23,14 +23,19 @@ public class Blaster_Aim : MonoBehaviour
     public Sprite ml;
     public Sprite empty;
 
+    public bool canShoot;
+    private float cooldown = 0;
+
     void Start()
     {
         currentWeapon = Weapons.NONE;
+        canShoot = true;
     }
 
     void Update()
     {
         //Rotation Call
+        float delta = Time.time * 1000;
         RotationWeapon(Camera.main.ScreenToWorldPoint(Input.mousePosition));
 
         //Weapon Select
@@ -63,28 +68,72 @@ public class Blaster_Aim : MonoBehaviour
         //Weapon Use
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            if (currentWeapon == Weapons.WPN1)
+            if (currentWeapon == Weapons.WPN1 && canShoot)
             {
                 GameObject newRocket = Instantiate(rocket, transform.position + transform.right, transform.rotation);
                 newRocket.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.right * rocketSpeed);
                 FindObjectOfType<AudioManager>().Play("RocketLauncher");
+                canShoot = false;
             }
-            if (currentWeapon == Weapons.WPN2)
+            if (currentWeapon == Weapons.WPN2 && canShoot)
             {
                 GameObject newC4 = Instantiate(C4, transform.position + transform.right, transform.rotation);
                 newC4.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.right * C4Speed);
                 FindObjectOfType<AudioManager>().Play("C4");
+                canShoot = false;
             }
-            if (currentWeapon == Weapons.WPN3)
+            if (currentWeapon == Weapons.WPN3 && canShoot)
             {
                 GameObject newGrenade = Instantiate(grenade, transform.position + transform.right, transform.rotation);
                 newGrenade.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.right * GrenadeSpeed);
                  FindObjectOfType<AudioManager>().Play("GrenadeLauncher");
+                canShoot = false;
             }
-            if (currentWeapon == Weapons.WPN4)
+            if (currentWeapon == Weapons.WPN4 && canShoot)
             {
                 GameObject newMine = Instantiate(mine, transform.position + transform.right, transform.rotation);
                 newMine.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.right * MineSpeed);
+                canShoot = false;
+            }
+        }
+        //Weapon Delay
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (currentWeapon == Weapons.WPN1)
+            {
+                cooldown += delta;
+                if (cooldown > 10000)
+                {
+                    canShoot = true;
+                    cooldown = 0;
+                }
+            }
+            if (currentWeapon == Weapons.WPN2)
+            {
+                cooldown += delta;
+                if (cooldown > 10000)
+                {
+                    canShoot = true;
+                    cooldown = 0;
+                }
+            }
+            if (currentWeapon == Weapons.WPN3)
+            {
+                cooldown += delta;
+                if (cooldown > 10000)
+                {
+                    canShoot = true;
+                    cooldown = 0;
+                }
+            }
+            if (currentWeapon == Weapons.WPN4)
+            {
+                cooldown += delta;
+                if (cooldown > 10000)
+                {
+                    canShoot = true;
+                    cooldown = 0;
+                }
             }
         }
     }
