2d9e58c97854a0005d74ec0557a875f4e505280a
Miquel Nadal <miquel.nadal@enti.cat>
2021-05-19 11:45:31 +0200

    Torreta Nuvea :)

---
+++ Assets/Scripts/Attack_Cone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack_Cone : MonoBehaviour
+{
+    public New_Turret_Script turretAI;
+
+    public bool isLeft = false;
+
+    void Awake()
+    {
+        turretAI = gameObject.GetComponentInParent<New_Turret_Script>();
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            if (isLeft)
+            {
+                turretAI.Attack(false);
+            }
+            else
+            {
+                turretAI.Attack(true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/New_Turret_Script.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class New_Turret_Script : MonoBehaviour
+{
+    public float distance;
+    public float wakeRange;
+    public float shootInterval;
+    public float bulletSpeed = 100;
+    public float bulletTimer;
+
+    public GameObject bullet;
+    public Transform target;
+    public Animator anim;
+    public Transform shootPointLeft;
+    public Transform shootPointRight;
+
+    public bool awake = false;
+    public bool lookingRight = false;
+
+
+     void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+     void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        anim.SetBool("Awake", awake);
+        anim.SetBool("Looking_Right", lookingRight);
+
+        RangeCheck();
+
+        if(target.transform.position.x > transform.position.x)
+        {
+            lookingRight = true;
+        }
+        if (target.transform.position.x < transform.position.x)
+        {
+            lookingRight = false;
+        }
+    }
+
+
+    void RangeCheck()
+    {
+        distance = Vector3.Distance(transform.position, target.transform.position);
+
+        if(distance < wakeRange)
+        {
+            awake = true;
+        }
+        if(distance > wakeRange)
+        {
+            awake = false;
+        }
+    }
+
+
+    public void Attack(bool attackingRight)
+    {
+        bulletTimer += Time.deltaTime;
+
+        if(bulletTimer >= shootInterval)
+        {
+            Vector2 direction = target.transform.position - transform.position;
+            direction.Normalize();
+
+            if (!attackingRight)
+            {
+                GameObject clone;
+                clone = Instantiate(bullet, shootPointLeft.transform.position, shootPointLeft.transform.rotation);
+                clone.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+
+                bulletTimer = 0;
+            }
+
+            if (attackingRight)
+            {
+                GameObject clone;
+                clone = Instantiate(bullet, shootPointRight.transform.position, shootPointRight.transform.rotation);
+                clone.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+
+                bulletTimer = 0;
+            }
+
+        }
+    }
+}
+
