d165afed68597c15ac23c0b95f991f3d868b291a
Ernest Gomis <gxmis791@gmail.com>
2021-06-02 13:22:01 +0200

    PopUp Funcionando de puta madre e implementados en todos los niveles :D

--- Assets/Scripts/PopUpText.cs
+++ Assets/Scripts/PopUpText.cs
@@ -1,18 +1,24 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PopUpText : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject text;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.tag == "Player")
+        {
+            text.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            text.SetActive(false);
+        }
     }
 }
