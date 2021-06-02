c941a78ba8d5c4da5aaa40f1b5cfacca3bfa0176
Sara <sara.perez@enti.cat>
2021-06-02 18:03:57 +0200

    Script Score Canvas

--- Assets/Scripts/ScoreCanvasManager.cs
+++ Assets/Scripts/ScoreCanvasManager.cs
@@ -1,10 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ScoreCanvasManager : MonoBehaviour
 {
     [SerializeField] Animator starsAnimator;
+    public Text score;
+    public GameObject tiempotext;
+
+    private void Start()
+    {
+        tiempotext = GameObject.FindGameObjectWithTag("Score");
+        score = tiempotext.GetComponent<Text>();
+    }
 
     public void EndLevel(int stars)
     {
@@ -14,14 +24,16 @@ public class ScoreCanvasManager : MonoBehaviour
     public void GoToMenu()
     {
         Debug.LogError("Volver al menu");
+        SceneManager.LoadScene("Main_Menú");
     }
     public void Restart()
     {
-        Debug.LogError("Volver al menu");
+        Debug.LogError("Reiniciar nivel");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     public void NextLevel()
     {
-        Debug.LogError("Volver al menu");
+        Debug.LogError("Siguiente nivel");
         Game_Manager.Instance.LoadNextScene();
     }
 }
